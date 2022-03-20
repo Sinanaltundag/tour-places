810cda284e5233ef72415e4a523b837e257ee559
peykani <peykani@gmail.com>
2022-03-20 20:48:34 +0300

    clean code fix

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import './App.css';
-import Header from './components/header/Header';
-import Card from './components/cards/Card';
-import NavBar from './components/navbar/Navbar';
-import { data } from './helper/data';
-
+import "./App.css";
+import Header from "./components/header/Header";
+import Card from "./components/cards/Card";
+import NavBar from "./components/navbar/Navbar";
+import { data } from "./helper/data";
 
 function App() {
   return (
     <div className="App bg-info ">
-<NavBar/>
+      <NavBar />
       <Header />
-      <div id="image-container" className="row row-cols-1 row-cols-md-2 row-cols-xl-3 p-3">
-{data.map((cardItem)=>{
-  return (
 
-      <Card title={cardItem.title.toUpperCase()} imgSrc= {cardItem.image}  desc= {cardItem.desc}/>
- 
-  )
-})}
-     </div>
+      <Card cards={data} />
     </div>
   );
 }
 
 export default App;
+
+// {/* <Card title={cardItem.title.toUpperCase()} imgSrc= {cardItem.image}  desc= {cardItem.desc}/> */}
--- src/components/cards/Card.js
+++ src/components/cards/Card.js
@@ -1,7 +1,7 @@
 import React from "react";
-import "./Card.css"
+import "./Card.css";
 function Card(props) {
-/* const [isOver,setIsOver]= useState(false)
+  /* const [isOver,setIsOver]= useState(false)
 
 const handleOver=(em)=>{
     console.log(em);
@@ -12,28 +12,24 @@ setIsOver(newPrgf)
 } */
 
   return (
-    
-
-      <div>
-        <div className="card bg-dark text-white text-start p-3 m-3"  /* onMouseOver={()=>handleOver(cardItem)} */>
-        <h2 className="card-title my-3 ">{props.title}</h2>
-        <img
-          className="card-img"
-          src={props.imgSrc}
-          alt="nice place"
-        />
-        <div className="overlay">
-          <p className="card-img-desc">
-            {props.desc}
-          </p>
+    <div
+      id="image-container"
+      className="row row-cols-1 row-cols-md-2 row-cols-xl-3 g-5 "
+    >
+      {props.cards.map((card) => (
+        <div className="col">
+          <div
+            className="card bg-dark text-white text-start p-3"
+            key={card.id} /* onMouseOver={()=>handleOver(cardItem)} */
+          >
+            <h2 className="card-title my-3 ">{card.title}</h2>
+            <img className="card-img" src={card.image} alt="nice place" />
+            <div className="overlay">
+              <p className="card-img-desc">{card.desc}</p>
+            </div>
+          </div>
         </div>
-    
-    
-
-
-
-     
-      </div>
+      ))}
     </div>
   );
 }
